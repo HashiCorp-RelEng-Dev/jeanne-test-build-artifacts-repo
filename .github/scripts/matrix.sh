e8f7afd70c23752105a7207da48bbabdbd312e46
Jeanne Franco <jeanne.franco@hashicorp.com>
2025-01-08 18:50:13 -0800

    upload artifacts then poll

--- .github/scripts/matrix.sh
+++ .github/scripts/matrix.sh
@@ -5,5 +5,5 @@ dir=$1
 
 # set matrix var to list of file names in a given directory
 matrix="$(find "$dir" -mindepth 1 -maxdepth 1 -printf "%P\n" | jq --slurp --compact-output --raw-input 'split("\n")[:-1]')"
-echo $matrix
-echo "done!"
+
+echo "matrix=${matrix}" | tee -a "${GITHUB_OUTPUT}"
