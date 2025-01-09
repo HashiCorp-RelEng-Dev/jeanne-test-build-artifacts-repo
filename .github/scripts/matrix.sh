aaf84b054ddf4bf726d3168051379f98bab74da8
Jeanne Franco <jeanne.franco@hashicorp.com>
2025-01-08 18:36:41 -0800

    test

---
+++ .github/scripts/matrix.sh
@@ -0,0 +1,9 @@
+!/usr/bin/env bash
+set -euo pipefail
+
+dir=$1
+
+# set matrix var to list of file names in a given directory
+matrix="$(find "$dir" -mindepth 1 -maxdepth 1 -printf "%P\n" | jq --slurp --compact-output --raw-input 'split("\n")[:-1]')"
+echo $matrix
+echo "done!"
