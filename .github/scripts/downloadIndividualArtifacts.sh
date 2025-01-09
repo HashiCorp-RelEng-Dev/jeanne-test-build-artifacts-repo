259d3588c5e6e5b9c278c81348e201d1797457e2
Jeanne Franco <jeanne.franco@hashicorp.com>
2025-01-09 10:41:11 -0800

    update ent download

--- .github/scripts/downloadIndividualArtifacts.sh
+++ .github/scripts/downloadIndividualArtifacts.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 filename=$1
-PRODUCT=$(curl -L https://api.releases.hashicorp.com/v1/releases/vault/latest | jq -r '.name')
-VERSION=$(curl -L https://api.releases.hashicorp.com/v1/releases/vault/latest | jq -r '.version')
+
+if [[ "$filename" == *ent* ]]; then
+    PRODUCT=$(curl -L https://api.releases.hashicorp.com/v1/releases/vault/1.18.3+ent | jq -r '.name')
+    VERSION=$(curl -L https://api.releases.hashicorp.com/v1/releases/vault/1.18.3+ent | jq -r '.version')
+else
+    PRODUCT=$(curl -L https://api.releases.hashicorp.com/v1/releases/vault/latest | jq -r '.name')
+    VERSION=$(curl -L https://api.releases.hashicorp.com/v1/releases/vault/latest | jq -r '.version')
+fi
+
 OUTPUT_DIR="artifacts"
 mkdir -p "$OUTPUT_DIR"
 
